2aa1d4baf0c2f27030687ba5a6e9e51cce5f9cd3
ha.vuthanh <ha.vuthanh@DESKTOP-RRT6UU6.mobifone.vn>
2021-01-25 12:07:32 +0700

    test

--- WebScraping/src/upwork/haworker25/Main.java
+++ WebScraping/src/upwork/haworker25/Main.java
@@ -3,7 +3,7 @@ package upwork.haworker25;
 public class Main {
 	public static void main(String s[]) {
 		String url="";
-		UserGUI gui = new UserGUI(url);
+		UserGUI gui = new UserGUI(url); //
 	}
 	
 }
